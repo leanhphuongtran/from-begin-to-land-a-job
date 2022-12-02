f9a4391d608ec1c8a720b8acc4329f4c0e0dc49b
Le Anh Phuong Tran <75240836+leanhphuongtran@users.noreply.github.com>
2022-12-01 16:37:01 -0800

    Create array1_0.1.cpp

---
+++ array/array1_0.1.cpp
@@ -0,0 +1,28 @@
+// Topic: Array
+// Purpose: Calculate the average of a number of integer variables
+// Ver: 0.1
+// The number of the integer variable is decided by users
+// Language: C++
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main (void)
+{
+	int total = 0;
+	int count = 0;	
+	int number;
+
+	printf( "How many integer variables you want to calculate the average: " );
+	cin >> count;
+	for (int i = 0; i < count; i++) {
+		
+		printf( "Enter number: ");
+		cin >> number;
+		total += number;
+	}
+	float averNum = (float) total / count;
+	printf( "The average is: %f\n", averNum);
+}
