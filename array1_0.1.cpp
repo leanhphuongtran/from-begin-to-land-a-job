e9445098626950a6fbd6cb6e1a665ee00fa57853
Le Anh Phuong Tran <75240836+leanhphuongtran@users.noreply.github.com>
2022-12-01 16:29:13 -0800

    Update array1_0.1.cpp

--- array1_0.1.cpp
+++ array1_0.1.cpp
@@ -12,8 +12,8 @@ using namespace std;
 int main (void)
 {
 	int total = 0;
-int count = 0;	
-int number;
+	int count = 0;	
+	int number;
 
 	printf( "How many integer variables you want to calculate the average: " );
 	cin >> count;
@@ -26,4 +26,3 @@ int number;
 	float averNum = (float) total / count;
 	printf( "The average is: %f\n", averNum);
 }
-
